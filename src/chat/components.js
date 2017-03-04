1491273fa427d80400d7798adfc6c966f3827026
2005508097 <gokhansaman@gmail.com>
2017-03-04 11:13:50 +0300

    Initial sketch

--- src/chat/components.js
+++ src/chat/components.js
@@ -1,9 +1,9 @@
-require('./chat-boxes/chat-boxes-container');
-require('./chat-boxes/chat-box');
+//require('./chat-boxes/chat-boxes-container');
+//require('./chat-boxes/chat-box');
 require('./menu');
-require('./panes/chat-pane');
-require('./panes/mother-pane');
+//require('./panes/chat-pane');
+//require('./panes/mother-pane');
 require('./root');
-require('./thread/thread-list');
-require('./thread/thread-preview');
-require('./thread/threads-popover');
+//require('./thread/thread-list');
+//require('./thread/thread-preview');
+//require('./thread/threads-popover');
--- src/chat/menu/controller.js
+++ src/chat/menu/controller.js
@@ -1,11 +1,5 @@
 module.exports = class MenuController {
-    constructor(ChatService) {
-        ChatService.bind(ChatService.EventType.UPDATE, () => {
-            this.count = ChatService.getUnreadCount();
-        });
-
-        ChatService.bind(ChatService.EventType.SET_ACTIVE_THREAD, () => {
-            this.count = ChatService.getUnreadCount();
-        });
+    constructor() {
+        console.log("Hello world ------");
     }
 }

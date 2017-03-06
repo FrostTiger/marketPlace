323737b5248d9e374d8c2c4636ba04bdec6c5736
2005508097 <gokhansaman@gmail.com>
2017-03-06 22:31:02 +0300

    Add left menu and redefine menu item via using flexbox

--- src/chat/components.js
+++ src/chat/components.js
@@ -1,8 +1,8 @@
 //require('./chat-boxes/chat-boxes-container');
 //require('./chat-boxes/chat-box');
 require('./menu');
-//require('./panes/chat-pane');
-//require('./panes/mother-pane');
+require('./panes/chat-pane');
+require('./panes/mother-pane');
 require('./root');
 //require('./thread/thread-list');
 //require('./thread/thread-preview');

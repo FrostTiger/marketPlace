84a8cc93f90d1951def6775625ead4fe2f03718d
2005508097 <gokhansaman@gmail.com>
2017-03-11 23:00:15 +0300

    add live adding function on shopping list

--- src/chat/chat-boxes/shopping-card/controller.js
+++ src/chat/chat-boxes/shopping-card/controller.js
@@ -3,5 +3,10 @@ module.exports = class ShoppingCardController {
         console.log("ShoppingCard enabled");
         this.items=["Domates","Biber","Salatalık","Yumurta"];
     }
+    addItem(item) {
+        console.log("ekleme denenmiştir.");
+        this.items.push(item);
+        console.log(items[4]);
+    }
 
 }

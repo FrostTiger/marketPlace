4aaf700a8949ba492ff99d5aeef41a232f2dc211
2005508097 <gokhansaman@gmail.com>
2017-03-18 20:02:23 +0300

    Adapt shooping card items style for new mock-backend.js data and redefine add function.

--- src/chat/chat-boxes/shopping-card/controller.js
+++ src/chat/chat-boxes/shopping-card/controller.js
@@ -1,18 +1,9 @@
 module.exports = class ShoppingCardController {
     constructor() {
-
-        
-        console.log('shop den',this.items);
      
-}
-    addItem(item) {
-        console.log("ekleme denenmiştir.");
-        this.items.push(item);
-        console.log(items[4]);
     }
-      setCard(Card){
-        this.name=Card.name;
-        this.items=Card.items;
-    } 
+    addItem(item) {
 
+        this.card.items.push([item,false]);
+    }
 }

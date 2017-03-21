b08c76cff7c3269f112124b5dba44e6f72d926bf
2005508097 <gokhansaman@gmail.com>
2017-03-21 12:21:04 +0300

    Update style of remove item button in shopping card

--- src/chat/chat-boxes/shopping-card/controller.js
+++ src/chat/chat-boxes/shopping-card/controller.js
@@ -1,26 +1,26 @@
 module.exports = class ShoppingCardController {
-    constructor(CardService,ChatService) {
-     this.cardService=CardService;
-     this.setArchiveScreen=CardService.archivedCard;
-     ChatService.owner.then(owner => {
-        this.owner = owner
+    constructor(CardService, ChatService) {
+        this.cardService = CardService;
+        this.setArchiveScreen = CardService.archivedCard;
+        ChatService.owner.then(owner => {
+            this.owner = owner
             console.log('owner değeri', this.owner.cards);
         });
-        CardService.bind('ArchiveScreen',() =>{
-        this.setArchiveScreen=CardService.archiveScreen;
-        }) 
+        CardService.bind('ArchiveScreen', () => {
+            this.setArchiveScreen = CardService.archiveScreen;
+        })
     }
     addItem(item) {
 
-        this.card.items.push([item,false]);
+        this.card.items.push([item, false]);
     }
-    deleteCard(cardName){
+    deleteCard(cardName) {
         this.cardService.setDeleteCard(cardName);
     }
-    archivedCard(cardName){
+    archivedCard(cardName) {
         this.cardService.setarchivedCard(cardName);
     }
-    removeItem(index){
-        this.card.items.splice(index,1);
+    removeItem(index) {
+        this.card.items.splice(index, 1);
     }
 }

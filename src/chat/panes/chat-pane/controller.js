4bf6a64d817c0bf92f167266418f2574c4f853ee
kubragizem <kubragizem@sabanciuniv.edu>
2017-03-22 23:47:08 +0300

    Update content pane for search function

--- src/chat/panes/chat-pane/controller.js
+++ src/chat/panes/chat-pane/controller.js
@@ -1,14 +1,20 @@
 module.exports = class ChatPaneController {
-    constructor(ChatService,CardService) {
-    this.setArchiveScreen=CardService.archiveScreen;
-    ChatService.owner.then(owner => { this.owner = owner    
-    });
-       CardService.bind('UptodateScreen',() => {
-        this.setArchiveScreen=CardService.archiveScreen;
-    }) 
-     CardService.bind('ArchiveScreen',() => {
-        this.setArchiveScreen=CardService.archiveScreen;
-        }) 
+    constructor(ChatService, CardService) {
+        this.setArchiveScreen = CardService.archiveScreen;
+        this.setSearchItem = CardService.searchItem;
+        this.setSearchScreen = CardService.searchScreen;
+        ChatService.owner.then(owner => {
+        this.owner = owner
+        });
+        CardService.bind('UptodateScreen', () => {
+            this.setArchiveScreen = CardService.archiveScreen;
+        })
+        CardService.bind('ArchiveScreen', () => {
+            this.setArchiveScreen = CardService.archiveScreen;
+        })
+        CardService.bind('SearchScreen', () => {
+            this.setSearchItem = CardService.searchItem;
+            this.setSearchScreen = CardService.searchScreen;
+        })
     }
-
 }

7c9419fb30e3a40c993e56012efc733be2c55355
ezgicatalbas <ezgicatalbas@sabanciuniv.edu>
2017-03-26 23:11:48 +0300

    Add MyProfile screen on top menu Users name, surname, email, phone, adress, picture informations are added.

--- src/chat/panes/mother-pane/controller.js
+++ src/chat/panes/mother-pane/controller.js
@@ -1,5 +1,32 @@
 module.exports = class MotherPaneController {
-    constructor() {
+    constructor(CardService, ChatService) {
+        this.cardService = CardService;
+        this.archiveScreen = CardService.archiveScreen;
+        this.uptodateScreen = CardService.uptodateScreen;
+        this.setSearchItem = CardService.searchItem;
+        this.setSearchScreen = CardService.searchScreen;
+        this.recycleBinScreen = CardService.recycleBinScreen;
+        this.profileScreen = CardService.profileScreen;
+        this.userScreen = CardService.userScreen;
+        CardService.bind('UptodateScreen', () => {
+            this.uptodateScreen = CardService.uptodateScreen;
+        })
+        CardService.bind('ArchiveScreen', () => {
+            this.archiveScreen = CardService.archiveScreen;
+        })
+        CardService.bind('SearchScreen', () => {
+            this.setSearchItem = CardService.searchItem;
+            this.setSearchScreen = CardService.searchScreen;
+        })
+        CardService.bind('RecycleBin', () => {
+            this.recycleBinScreen = CardService.recycleBinScreen;
+        })
+        CardService.bind('User', () => {
+            this.userScreen = CardService.userScreen;
+        })
+        CardService.bind('Profile', () => {
+            this.profileScreen = CardService.profileScreen;
+        })
     }
 
 
